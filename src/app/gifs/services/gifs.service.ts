ec120366add7e1f2767ad126021400e7d25b6854
CoolTerran <marlon.orihuela@gmail.com>
2021-11-23 14:55:45 -0500

    primer commit nuevo repositorio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' //provideIn eleva a un nivel global el servicio y no es necesario declararlo en los
 })
 export class GifsService {
 
-  constructor() { }
+  private _historial: string[] = [];
+
+  
+  get historial() {
+    return [...this._historial]; //rompemos la referencia con [...] para proteger la propiedad original
+  }
+
+  buscarGifs(query: string = '') {
+
+    query = query.trim().toLocaleLowerCase();
+
+    if(!this._historial.includes(query)) {
+      this._historial.unshift(query);
+    }
+
+    this._historial = this._historial.splice(0,10);
+
+    console.log(this._historial);
+}
 }
